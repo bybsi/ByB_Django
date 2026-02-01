230af2a2e0095d722ad2bb42a7b30358e4ea3cfc
Brian Stull <thingbyb@gmail.com>
2026-02-01 16:52:37 -0500

    remove commented out code

--- thingbyb/users/backends.py
+++ thingbyb/users/backends.py
@@ -1,21 +1,16 @@
 from django.contrib.auth.backends import BaseBackend
 from . import module as user_module
 from .models import User
-from utils.decrypt import DBCrypt
 import re
 import os
 import hashlib
 
 class UserBackend(BaseBackend):
-    #client_id = DBCrypt(keyfile='.keys/gclid.key').decrypt('ieB1/9npI9J+SiyIwvapFlOqpqdAjV5WmzxfWszZcv5vAqqbNXP8jwhpoawEhbNvz7VXthwzUbF5ivj9303AUAKWPUtJNYB9zRG5zyrQXI0=')
-
 
     def authenticate(self, request, username=None, password=None):
-        #user = UserBackend.get_user(username)
         user = user_module.get_user(username)
         if user is None:
             return None
-#        if UserBackend.check_password_hash(password, user.password, user.salt):
         if user_module.check_password_hash(password, user.password, user.salt):
             return user
         return None
