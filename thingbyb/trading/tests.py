6a73c2b69ef924dcae3a0374b6c4a81a74cf4f47
Brian Stull <thingbyb@gmail.com>
2026-02-05 15:24:50 -0500

    simple tests for trading app

--- thingbyb/trading/tests.py
+++ thingbyb/trading/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from utils.redis import redis_get, redis_rpush
 
-# Create your tests here.
+class TradingTests(SimpleTestCase):
+    
+    def test_redis(self):
+        ex = redis_get('testkey')
+        print(f"{ex}")
+        ne = redis_get('nokey')
+        print(f"{ne}")
+        if redis_rpush('testq', 'testdata'):
+            print(f"pushed elem")
+        else:
+            print(f"didn't  push elem")
+
+        
